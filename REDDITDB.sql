2307c7acf84ebc4dfd20f3a10f89d71192a7ba61
Thần tài đến <nguyenphuk48@gmail.com>
2024-12-05 22:22:06 +0700

    Add procedure to increase contribution points and update account rank - Add trigger to update gold

--- REDDITDB.sql
+++ REDDITDB.sql
@@ -465,3 +465,63 @@ CREATE TABLE TaiKhoan_Gui_BaoCao (
     FOREIGN KEY (MaTaiKhoan) REFERENCES TaiKhoan(MaTaiKhoan),
     FOREIGN KEY (MaBaoCao) REFERENCES BaoCao(MaBaoCao) 
 ) TABLESPACE tb_index;
+
+-- Thủ tục tăng điểm đóng góp và cập nhật hạng tài khoản 
+CREATE OR REPLACE PROCEDURE CapNhatDiemDongGop IS
+BEGIN
+    -- Tăng điểm đóng góp dựa trên bảng  TaiKhoan_Dang_BaiDang
+    UPDATE TaiKhoan T
+    SET T.DiemDongGop = T.DiemDongGop + 
+        (SELECT COUNT(*)
+         FROM TaiKhoan_Dang_BaiDang D
+         WHERE D.MaTaiKhoan = T.MaTaiKhoan);
+         
+    -- Cập nhật hạng tài khoản 
+    UPDATE TaiKhoan
+    SET HangTaiKhoan = HangTaiKhoan + 1
+    WHERE DiemDongGop >= 2;
+    
+    commit;
+END;
+/
+
+
+
+----6: Cap nhat vang sau khi nap tien
+CREATE OR REPLACE TRIGGER CapNhatSoVangTaiKhoan
+AFTER INSERT ON TaiKhoan_NapTien_TaiKhoan
+FOR EACH ROW
+BEGIN
+    UPDATE TaiKhoan
+    SET Vang = Vang + :NEW.SoVangNhanDuoc
+    WHERE MaTaiKhoan = :NEW.MaTaiKhoan;
+END;
+/
+
+exec CapNhatDiemDongGop;
+-----
+--
+/*
+CREATE OR REPLACE TRIGGER CapNhatHangTaiKhoan
+AFTER UPDATE OF DiemDongGop ON TaiKhoan
+FOR EACH ROW
+BEGIN
+    
+    IF :NEW.DiemDongGop >= 2 AND :OLD.DiemDongGop < 2 THEN
+        UPDATE TaiKhoan
+        SET HangTaiKhoan = HangTaiKhoan + 1  
+        WHERE MaTaiKhoan = :NEW.MaTaiKhoan;
+    
+    END IF;
+END;
+/
+CREATE OR REPLACE TRIGGER TangDiemDongGop
+AFTER INSERT ON TaiKhoan_Dang_BaiDang
+FOR EACH ROW
+BEGIN
+    UPDATE TaiKhoan
+    SET DiemDongGop = DiemDongGop + 1
+    WHERE MaTaiKhoan = :NEW.MaTaiKhoan;
+END;
+/
+*/
