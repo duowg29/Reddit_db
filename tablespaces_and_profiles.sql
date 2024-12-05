733845de158089a1c7be36f8cb84b382b382b15d
hqzero001 <hungstar89@gmail.com>
2024-12-05 17:58:32 +0700

    update profiles in tablespaces_and_profiles.sql

--- tablespaces_and_profiles.sql
+++ tablespaces_and_profiles.sql
@@ -10,7 +10,7 @@ MAXSIZE UNLIMITED;
 -- Tạo Tablespace index
 -- Lưu ý: vị trí datafile tự tạo nhé :)
 CREATE TABLESPACE tb_index
-DATAFILE 'C:/BA/Year 3 Semester 1/6 He quan tri co so du lieu/Bai tap lon/reddit_dbms_github/Reddit_db/tablespace/index_ts.dbf' 
+DATAFILE 'C:/BA/Year 3 Semester 1/6 He quan tri co so du lieu/Bai tap lon/reddit_dbms_github/Reddit_db/tablespace/tb_index.dbf' 
 SIZE 100M 
 AUTOEXTEND ON 
 NEXT 10M 
@@ -19,7 +19,7 @@ MAXSIZE UNLIMITED;
 -- Tạo Temporary Tablespace
 -- Lưu ý: vị trí datafile tự tạo nhé :)
 CREATE TEMPORARY TABLESPACE tb_user_temp 
-TEMPFILE 'C:/BA/Year 3 Semester 1/6 He quan tri co so du lieu/Bai tap lon/reddit_dbms_github/Reddit_db/tablespace/tempt_ts.dbf' 
+TEMPFILE 'C:/BA/Year 3 Semester 1/6 He quan tri co so du lieu/Bai tap lon/reddit_dbms_github/Reddit_db/tablespace/tb_temp.dbf' 
 SIZE 50M 
 AUTOEXTEND ON 
 NEXT 5M 
@@ -30,7 +30,6 @@ MAXSIZE UNLIMITED;
 -- Tạo profile cho db_admin
 CREATE PROFILE C##db_admin_profile LIMIT
   SESSIONS_PER_USER        UNLIMITED,
-  CPU_PER_SESSION          UNLIMITED,
   CONNECT_TIME             UNLIMITED,
   IDLE_TIME                120,
   FAILED_LOGIN_ATTEMPTS    5,
@@ -41,7 +40,6 @@ CREATE PROFILE C##db_admin_profile LIMIT
 -- Tạo profile cho db_developer
 CREATE PROFILE C##db_developer_profile LIMIT
   SESSIONS_PER_USER        3,
-  CPU_PER_SESSION          10000,
   CONNECT_TIME             180,
   IDLE_TIME                60,
   FAILED_LOGIN_ATTEMPTS    5,
@@ -52,7 +50,6 @@ CREATE PROFILE C##db_developer_profile LIMIT
 -- Tạo profile cho backend_developer
 CREATE PROFILE C##backend_developer_profile LIMIT
   SESSIONS_PER_USER        3,
-  CPU_PER_SESSION          10000,
   CONNECT_TIME             180,
   IDLE_TIME                60,
   FAILED_LOGIN_ATTEMPTS    5,
@@ -63,7 +60,6 @@ CREATE PROFILE C##backend_developer_profile LIMIT
 -- Tạo profile cho data_engineer
 CREATE PROFILE C##data_engineer_profile LIMIT
   SESSIONS_PER_USER        4,
-  CPU_PER_SESSION          20000,
   CONNECT_TIME             240,
   IDLE_TIME                60,
   FAILED_LOGIN_ATTEMPTS    5,
@@ -74,7 +70,6 @@ CREATE PROFILE C##data_engineer_profile LIMIT
 -- Tạo profile cho data_analyst
 CREATE PROFILE C##data_analyst_profile LIMIT
   SESSIONS_PER_USER        3,
-  CPU_PER_SESSION          10000,
   CONNECT_TIME             180,
   IDLE_TIME                60,
   FAILED_LOGIN_ATTEMPTS    5,
@@ -85,8 +80,6 @@ CREATE PROFILE C##data_analyst_profile LIMIT
 -- Tạo profile cho supervisor
 CREATE PROFILE C##supervisor_profile LIMIT
   SESSIONS_PER_USER        5,
-  CPU_PER_SESSION          20000,
-  CPU_PER_CALL             2000,
   CONNECT_TIME             240,
   IDLE_TIME                60,
   FAILED_LOGIN_ATTEMPTS    3,
@@ -97,7 +90,6 @@ CREATE PROFILE C##supervisor_profile LIMIT
 -- Tạo profile cho end_user
 CREATE PROFILE C##end_user_profile LIMIT
   SESSIONS_PER_USER        1,
-  CPU_PER_SESSION          5000,
   CONNECT_TIME             60,
   IDLE_TIME                15,
   FAILED_LOGIN_ATTEMPTS    3;
