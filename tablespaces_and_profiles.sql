abf799371d60c0ee6dbf97f8625f2c4e98edb63c
hqzero001 <hungstar89@gmail.com>
2024-12-05 15:18:23 +0700

    fix common user prefix in tablespaces_and_profiles.sql

--- tablespaces_and_profiles.sql
+++ tablespaces_and_profiles.sql
@@ -28,76 +28,76 @@ MAXSIZE UNLIMITED;
 -- Tạo Profile
 
 -- Tạo profile cho db_admin
-CREATE PROFILE db_admin_profile LIMIT
-  SESSIONS_PER_USER        UNLIMITED
-  CPU_PER_SESSION          UNLIMITED
-  CONNECT_TIME             UNLIMITED
-  IDLE_TIME                120
-  FAILED_LOGIN_ATTEMPTS    5
-  PASSWORD_LIFE_TIME       90
-  PASSWORD_REUSE_TIME      365
+CREATE PROFILE C##db_admin_profile LIMIT
+  SESSIONS_PER_USER        UNLIMITED,
+  CPU_PER_SESSION          UNLIMITED,
+  CONNECT_TIME             UNLIMITED,
+  IDLE_TIME                120,
+  FAILED_LOGIN_ATTEMPTS    5,
+  PASSWORD_LIFE_TIME       90,
+  PASSWORD_REUSE_TIME      365,
   PASSWORD_REUSE_MAX       5;
 
 -- Tạo profile cho db_developer
-CREATE PROFILE db_developer_profile LIMIT
-  SESSIONS_PER_USER        3
-  CPU_PER_SESSION          10000
-  CONNECT_TIME             180
-  IDLE_TIME                60
-  FAILED_LOGIN_ATTEMPTS    5
-  PASSWORD_LIFE_TIME       90
-  PASSWORD_REUSE_TIME      180
+CREATE PROFILE C##db_developer_profile LIMIT
+  SESSIONS_PER_USER        3,
+  CPU_PER_SESSION          10000,
+  CONNECT_TIME             180,
+  IDLE_TIME                60,
+  FAILED_LOGIN_ATTEMPTS    5,
+  PASSWORD_LIFE_TIME       90,
+  PASSWORD_REUSE_TIME      180,
   PASSWORD_REUSE_MAX       5;
 
 -- Tạo profile cho backend_developer
-CREATE PROFILE backend_developer_profile LIMIT
-  SESSIONS_PER_USER        3
-  CPU_PER_SESSION          10000
-  CONNECT_TIME             180
-  IDLE_TIME                60
-  FAILED_LOGIN_ATTEMPTS    5
-  PASSWORD_LIFE_TIME       90
-  PASSWORD_REUSE_TIME      180
+CREATE PROFILE C##backend_developer_profile LIMIT
+  SESSIONS_PER_USER        3,
+  CPU_PER_SESSION          10000,
+  CONNECT_TIME             180,
+  IDLE_TIME                60,
+  FAILED_LOGIN_ATTEMPTS    5,
+  PASSWORD_LIFE_TIME       90,
+  PASSWORD_REUSE_TIME      180,
   PASSWORD_REUSE_MAX       5;
 
 -- Tạo profile cho data_engineer
-CREATE PROFILE data_engineer_profile LIMIT
-  SESSIONS_PER_USER        4
-  CPU_PER_SESSION          20000
-  CONNECT_TIME             240
-  IDLE_TIME                60
-  FAILED_LOGIN_ATTEMPTS    5
-  PASSWORD_LIFE_TIME       90
-  PASSWORD_REUSE_TIME      180
+CREATE PROFILE C##data_engineer_profile LIMIT
+  SESSIONS_PER_USER        4,
+  CPU_PER_SESSION          20000,
+  CONNECT_TIME             240,
+  IDLE_TIME                60,
+  FAILED_LOGIN_ATTEMPTS    5,
+  PASSWORD_LIFE_TIME       90,
+  PASSWORD_REUSE_TIME      180,
   PASSWORD_REUSE_MAX       5;
 
 -- Tạo profile cho data_analyst
-CREATE PROFILE data_analyst_profile LIMIT
-  SESSIONS_PER_USER        3
-  CPU_PER_SESSION          10000
-  CONNECT_TIME             180
-  IDLE_TIME                60
-  FAILED_LOGIN_ATTEMPTS    5
-  PASSWORD_LIFE_TIME       90
-  PASSWORD_REUSE_TIME      180
+CREATE PROFILE C##data_analyst_profile LIMIT
+  SESSIONS_PER_USER        3,
+  CPU_PER_SESSION          10000,
+  CONNECT_TIME             180,
+  IDLE_TIME                60,
+  FAILED_LOGIN_ATTEMPTS    5,
+  PASSWORD_LIFE_TIME       90,
+  PASSWORD_REUSE_TIME      180,
   PASSWORD_REUSE_MAX       5;
 
 -- Tạo profile cho supervisor
-CREATE PROFILE supervisor_profile LIMIT
-  SESSIONS_PER_USER        5
-  CPU_PER_SESSION          20000
-  CPU_PER_CALL             2000
-  CONNECT_TIME             240
-  IDLE_TIME                60
-  FAILED_LOGIN_ATTEMPTS    3
-  PASSWORD_LIFE_TIME       60
-  PASSWORD_REUSE_TIME      365
+CREATE PROFILE C##supervisor_profile LIMIT
+  SESSIONS_PER_USER        5,
+  CPU_PER_SESSION          20000,
+  CPU_PER_CALL             2000,
+  CONNECT_TIME             240,
+  IDLE_TIME                60,
+  FAILED_LOGIN_ATTEMPTS    3,
+  PASSWORD_LIFE_TIME       60,
+  PASSWORD_REUSE_TIME      365,
   PASSWORD_REUSE_MAX       10;
 
 -- Tạo profile cho end_user
-CREATE PROFILE end_user_profile LIMIT
-  SESSIONS_PER_USER        1
-  CPU_PER_SESSION          5000
-  CONNECT_TIME             60
-  IDLE_TIME                15
+CREATE PROFILE C##end_user_profile LIMIT
+  SESSIONS_PER_USER        1,
+  CPU_PER_SESSION          5000,
+  CONNECT_TIME             60,
+  IDLE_TIME                15,
   FAILED_LOGIN_ATTEMPTS    3;
